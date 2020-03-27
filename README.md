2a2ccb21fea7b00db46e0d44ad2dd2d9561746e3
Alan Berezin <alan.berezin1@gmail.com>
2020-03-27 11:48:35 -0700

    wip

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 A python package to create the SQL merge function for sqlServer
+see https://github.com/metrogistics/gensqlmerge
+
+You do not need to install nor run the gensqlmerge to use the generated sql.
--- gensqlmerge/gen.py
+++ gensqlmerge/gen.py
@@ -22,7 +22,7 @@ import sys
               help="The column names for the columns that are updated: Format is:\
                destColumnName:srcColumnName[:<options>] [,...N] where <options> ::= castdate")
 @click.option("--compare-columns-file", \
-              help="A file with one row per pair of columns. Each row is formatted like:\
+              help="A file with one row per pair of columns. Lines that have a '#' in them are ignored.  Each row is formatted like:\
                destColumnName:srcColumnName [:<options>] where <options> ::= castdate")
 @click.option("--insert-columns",default="id:newid() ,othercode:1",\
               help="These columns are only inserted, not updated. The insert-columns will usually include the columns used \
@@ -32,6 +32,10 @@ def main(user,src_database_name,dest_database_name,server, source_sql, dest_tabl
     if (compare_columns_file and compare_columns):
         eprint("you cannot set both compare-columns and compare-columns-file")
         exit(1);
+
+    if (on_condition is None):
+        eprint("You must specify an --on-condition .  See --help")
+        exit(1)
 
     dest_database_schema = "dbo"
     # envkey=src_database_name+'_pw'
@@ -44,10 +48,13 @@ def main(user,src_database_name,dest_database_name,server, source_sql, dest_tabl
     sql += f"USING ({source_sql}) as src\n"
     sql += f"ON {on_condition}\n"
     sql += f"WHEN MATCHED AND (1 = 0 \n"
+    sql += f"--BE CAREFUL comparing dates!  Safer to cast(mycol as Date) and compare that. \n"
 
     ccolumns = getCompareColumns(compare_columns, compare_columns_file)
     for ccol in ccolumns:
         dest_col,src_col = dest_src_split(ccol)
+        # TODO when the source is not nullable as when you call isNull(someCol,'foo') then we dont need all this.
+        # Also, when dest is not nullable we dont need those tests
         sql += f"or (dest.{dest_col} is null and src.{src_col} is not null) " \
         + f"or (dest.{dest_col} is not null and src.{src_col} is null)" \
         + f"or dest.{dest_col} <> src.{src_col}\n "
@@ -87,7 +94,7 @@ def main(user,src_database_name,dest_database_name,server, source_sql, dest_tabl
 def getCompareColumns(compare_columns, compare_columns_file):
     if (compare_columns_file):
         with open(compare_columns_file) as f:
-            return [x.strip() for x in f.readlines()]
+            return [x.strip() for x in f.readlines() if x.strip() != "" and not re.match("^\s*#.*", x)]
     else:
         return [icol for icol in re.split(r"\s*,\s*", compare_columns)]
 
